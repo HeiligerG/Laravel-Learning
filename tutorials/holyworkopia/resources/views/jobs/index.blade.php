6a55801d0c3d0af8eadfdc97ddf8873e94e9ea97
HeiligerG <gggigi111@gmail.com>
2025-01-07 20:13:30 +0100

    SeachView/Route

--- tutorials/holyworkopia/resources/views/jobs/index.blade.php
+++ tutorials/holyworkopia/resources/views/jobs/index.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
+    <div class="bg-blue-900 h-24 px-4 flex items-center justify-center text-white mb-4 rounded">
+        <x-search />
+    </div>
+
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse($jobs as $job)
                 <x-jobs-card :job="$job" />
