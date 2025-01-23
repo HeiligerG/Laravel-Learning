5bcf0acae48e8733a88d4cae683c847dbba9916f
HolyG <122292538+HeiligerG@users.noreply.github.com>
2025-01-23 16:13:31 +0100

    FixedDropDown

--- projects/FoodFlow/app/Http/Controllers/CategoryController.php
+++ projects/FoodFlow/app/Http/Controllers/CategoryController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Requests\Store\StoreCategoryRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CategoryController extends Controller
 {
-    public function store(StoreCategoryRequest $request): RedirectResponse
+    public function store(StoreCategoryRequest $request): JsonResponse
     {
         $category = Category::create($request->validated());
 
-        return redirect()->route('categories.store')  // Weiterleitung zur Kategorien-Liste
-        ->with('success', 'Kategorie erfolgreich hinzugefügt.');
+        return response()->json([
+            'id' => $category->id,
+            'name' => $category->name,
+            'success' => true,
+            'message' => 'Kategorie hinzugefügt'
+        ]);
     }
 }
--- projects/FoodFlow/app/Http/Controllers/LocationController.php
+++ projects/FoodFlow/app/Http/Controllers/LocationController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Store\StoreLocationRequest;
 use App\Models\Location;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LocationController extends Controller
 {
-    public function store(StoreLocationRequest $request): RedirectResponse
+    public function store(StoreLocationRequest $request): JsonResponse
     {
         $location = Location::create($request->validated());
 
-        return redirect()->route('locations.store')
-        ->with('success', 'Standort erfolgreich hinzugefügt.');
+        return response()->json([
+            'id' => $location->id,
+            'name' => $location->name,
+            'success' => true,
+            'message' => '  Standort hinzugefügt'
+        ]);
     }
 }
