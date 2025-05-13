72a6b4426ff7a845d595f9800e78023189add541
HeiligerG <gggigi111@gmail.com>
2025-05-13 16:20:31 +0200

    F2

--- ÜK-M295/modul-295-twitter-main/app/Http/Controllers/LoginController.php
+++ ÜK-M295/modul-295-twitter-main/app/Http/Controllers/LoginController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login() {
-        return response()->json(['errors' => ['general' => 'E-Mail oder Passwort falsch.']], 422);
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = $request->user(); // oder: Auth::user()
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'token' => $token
+            ]);
+        }
+
+        return response()->json([
+            'errors' => [
+                'general' => 'E-Mail oder Passwort falsch.'
+            ]
+        ], 422);
     }
 }
