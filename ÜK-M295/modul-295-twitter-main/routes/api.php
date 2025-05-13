ca7d3184b264b8a1d7fce6f5e88ed86e0ba6c283
HeiligerG <gggigi111@gmail.com>
2025-05-13 16:15:41 +0200

    F1

--- ÜK-M295/modul-295-twitter-main/routes/api.php
+++ ÜK-M295/modul-295-twitter-main/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TweetController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,5 @@ Route::get('/users/{id}', [UserController::class, 'show']);
 Route::get('/users/{id}/tweets', [UserController::class, 'tweets']);
 
 Route::get('/tweets', [TweetController::class, 'tweets']);
+
+Route::post('/login', [LoginController::class, 'login']);
