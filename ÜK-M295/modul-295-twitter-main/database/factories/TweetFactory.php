8555ad3151f404413d913a3184e4ab5e9036cbf4
HeiligerG <gggigi111@gmail.com>
2025-05-13 15:14:46 +0200

    D1

--- ÜK-M295/modul-295-twitter-main/database/factories/TweetFactory.php
+++ ÜK-M295/modul-295-twitter-main/database/factories/TweetFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 class TweetFactory extends Factory
 {
@@ -10,6 +11,7 @@ class TweetFactory extends Factory
     {
         return [
             'text' => fake()->words(rand(3, 25), true),
+            'user_id' => User::factory(),
             'likes' => $this->faker->numberBetween(0, 5000),
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
             'updated_at' => now(),
