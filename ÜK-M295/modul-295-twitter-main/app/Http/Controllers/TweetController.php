a5889a673bcb7bfd114f4414ff162e0f57061003
HeiligerG <gggigi111@gmail.com>
2025-05-13 15:44:56 +0200

    E1

--- ÜK-M295/modul-295-twitter-main/app/Http/Controllers/TweetController.php
+++ ÜK-M295/modul-295-twitter-main/app/Http/Controllers/TweetController.php
@@ -4,26 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+
+use App\Http\Resources\TweetResource;
+
 use App\Models\Tweet;
 
 class TweetController extends Controller
 {
     public function index()
     {
-        $tweets = Tweet::latest()
-                        ->take(100)
-                        ->get();
+        $tweets = Tweet::with('user')
+                    ->latest()
+                    ->take(100)
+                    ->get();
 
-        $tweets = $tweets->map(function ($tweet) {
-            $tweet->user = [
-                "id" => $tweet->user_id,
-                "name" => "Franzi Musterfrau"
-            ];
-            return $tweet;
-        });
-
-        return response()->json([
-            'data' => $tweets
-        ]);
+        return TweetResource::collection($tweets);
     }
 }
