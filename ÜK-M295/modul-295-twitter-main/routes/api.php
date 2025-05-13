8ba961aaa7b57f00d227fa70bc618cd11435baa1
HeiligerG <gggigi111@gmail.com>
2025-05-13 15:32:30 +0200

    D3

--- ÜK-M295/modul-295-twitter-main/routes/api.php
+++ ÜK-M295/modul-295-twitter-main/routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\TweetController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/users/{id}', [UserController::class, 'show']);
 
 Route::get('/tweets', [TweetController::class, 'index']);
